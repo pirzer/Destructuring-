8096abe7ec5f300f58f7f19f859cc55d26002a04
pirzer <pirzer1@yahoo.com>
2022-11-04 14:58:20 +0000

    destructuring exercise

--- destructuring.js
+++ destructuring.js
@@ -4,6 +4,14 @@
  */
 
 // Destructuring arrays
+let ages = [30, 26, 27];
+
+//  let john = ages[0];
+//  let mary = ages[1];
+//  let joe = ages[2];
+
+let [john, mary, joe] = ages;
+console.log(john, mary, joe);
 
 // Destructuring objects
 
